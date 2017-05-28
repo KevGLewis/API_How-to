9ee0fd2eb23d5cbdd8f658f5b8488d1adf394f00
Kevin Lewis <kev.glynne@gmail.com>
2017-05-28 00:24:19 -0400

    Update 3

---
+++ assets/js/search.js
@@ -0,0 +1,71 @@
+var apiKey = 'zXUuF4xob97q9Q9qtVWL6zJkgttptBj00gDvO8yh'
+
+function searchFood(event){
+    
+    var searchValue = document.getElementById("searchValueTwo").value;
+    var ndbNumb = document.getElementsByClassName("ndb_numb");
+    var resultDescriptions = document.getElementsByClassName("resultDescription")
+    
+    var req = new XMLHttpRequest();
+    req.open('GET', 'https://api.nal.usda.gov/ndb/search/?format=json&q='+ searchValue +'&sort=r&max=5&offset=0&api_key=' + apiKey, true)
+    req.addEventListener('load',function(){
+      if(req.status >= 200 && req.status < 400){
+        var response = JSON.parse(req.responseText);
+          for(var i = 0; i < resultDescriptions.length; i++){
+              resultDescriptions[i].textContent = response.list.item[i].name;
+              ndbNumb[i].textContent = response.list.item[i].ndbno;
+          }
+          
+      } else {
+        console.log("Error in network request: " + req.statusText);
+      }
+    });
+    req.send(null);
+    
+    event.preventDefault();
+}
+
+function searchFoodOne(event){
+    
+    var searchValue = document.getElementById("searchValue").value;
+    console.log(searchValue);
+    var resultValue = document.getElementById("resultValue");
+    
+    resultValue.textContent = 'Search Parameters are: ' + searchValue;
+    event.preventDefault();
+}
+
+function searchFoodThree(event){
+    
+    var searchValue = document.getElementById("searchValueThree").value;
+    var ndbNumb = document.getElementsByClassName("ndb_numb");
+    var resultDescriptions = document.getElementsByClassName("resultDescription")
+    var searchButtons = document.getElementsByClassName("button select")
+    
+    var req = new XMLHttpRequest();
+    req.open('GET', 'https://api.nal.usda.gov/ndb/search/?format=json&q='+ searchValue +'&sort=r&max=5&offset=0&api_key=' + apiKey, true)
+    req.addEventListener('load',function(){
+      if(req.status >= 200 && req.status < 400){
+        var response = JSON.parse(req.responseText);
+          for(var i = 0; i < resultDescriptions.length; i++){
+              resultDescriptions[i].textContent = response.list.item[i].name;
+              ndbNumb[i].textContent = response.list.item[i].ndbno;
+              searchButtons[i].setAttribute( "onclick", "buttonSelect(event, "+response.list.item[i].ndbno+")" );
+              
+          }
+          
+      } else {
+        console.log("Error in network request: " + req.statusText);
+      }
+    });
+    req.send(null);
+    
+    event.preventDefault();
+}
+
+
+function buttonSelect(event, param){
+    console.log(param);
+    event.preventDefault();
+}
+
